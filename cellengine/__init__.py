2db4bc087d80d5e48934737991148a017e8f2032
Gerrit <g.egnew@gmail.com>
2019-08-09 05:50:39 -0600

    initial configurations

--- cellengine/__init__.py
+++ cellengine/__init__.py
@@ -1,3 +1,16 @@
+import os
+import requests
+from requests_toolbelt import sessions
+
+BASE_URL = os.environ.get("CELLENGINE_DEVELOPMENT", None)
+# BASE_URL = "https://cellengine.com/api/v1/"
+# BASE_URL = "http://localhost:3000/api/v1/"
+
+session = sessions.BaseUrlSession(base_url=BASE_URL)
+# session.params = {}
+# session.params['base_url'] = BASE_URL
+
 from .client import Client
-# from .compensation import Compensation
-# from .fcsfile import FcsFile
+from .experiment import Experiment
+from .compensation import Compensation
+from .fcsfile import FcsFile
