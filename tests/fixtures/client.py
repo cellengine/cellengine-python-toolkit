4871b8ae647af30c2a3f6414d5b3f68bcd496eff
Gerrit Egnew <35822787+gegnew@users.noreply.github.com>
2019-09-05 15:12:35 -0600

    filter secrets from vcr cassette creation (#15)
    I also added a mocked `base_delete` test; if it becomes troublesome to remove secrets from the `test_base_delete.yaml`, we can remove it and rely on `test_base_delete_mock`.

--- tests/fixtures/client.py
+++ tests/fixtures/client.py
@@ -1,6 +1,7 @@
 import vcr
 import pytest
 import cellengine
+from conftest import client_vcr
 
 
 @pytest.fixture
@@ -8,6 +9,6 @@ def client():
     """Returns an authenticated Client object
     This fixture should be passed to all other fixtures to ensure
     that tests run on an authenticated session of CellEngine."""
-    with vcr.use_cassette('tests/cassettes/client.yaml'):
+    with client_vcr.use_cassette('tests/cassettes/client.yaml'):
         client = cellengine.Client(username='gegnew', password='testpass1')
         return client
