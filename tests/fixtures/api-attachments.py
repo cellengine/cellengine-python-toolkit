6a1d6bd618a2dc43eba8f94d3b9933e5a8e7efa9
gegnew <g.egnew@gmail.com>
2020-02-05 22:16:16 +0100

    modify base api methods in preparation for attachments

---
+++ tests/fixtures/api-attachments.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture(scope='session')
+def attachments():
+    attachments = [
+        {
+            '_id': '5e3a5abf62c76b4f1b207b5b',
+            'size': 20890,
+            'md5': '545e35f5cb5317529c6e9e3cf88bff6f',
+            'filename': 'config.h',
+            'experimentId': '5e26b3f94b14014f02b1ecda',
+            '__v': 0,
+        }
+    ]
+    return attachments
