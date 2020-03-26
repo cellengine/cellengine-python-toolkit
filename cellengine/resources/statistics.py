23f159fd0e5240546775a47dceee17fe152a2049
gegnew <gerrit.egnew@relayr.io>
2020-03-25 21:29:42 -0600

    Add docs to get_statistis method

--- cellengine/resources/statistics.py
+++ cellengine/resources/statistics.py
@@ -15,6 +15,42 @@ def get_statistics(
     percent_of: Union[str, List[str]] = None,
     population_ids: List[str] = None,
 ):
+    """
+    Helper method for requesting Statistics from CellEngine.
+
+    Required Args:
+        experiment_id: ID of experiment to request statistics for.
+        statistics: Statistical method to request. Any of "mean", "median", "quantile",
+            "mad" (median absolute deviation), "geometric_mean", "eventcount",
+            "cv", "stddev" or "percent" (case-insensitive).
+        q: int: quantile (required for "quantile" statistic)
+        channels: str or List[str]: for "mean", "median", "geometricMean", "cv",
+            "stddev", "mad" or "quantile" statistics. Names of
+            channels to calculate statistics for.
+    Optional Args:
+        annotations: bool: Include file annotations in output (defaults to False).
+        compensation_id: str: Compensation to use for gating and statistic calculation.
+            Defaults to uncompensated. Three special constants may be used:
+                0: Uncompensated
+                -1: File-Internal Compensation Uses the file's internal compensation
+                    matrix, if available. If not available, an error will be returned.
+                -2: Per-File Compensation Use the compensation assigned to each
+                    individual FCS file.
+        fcs_file_ids: List[str]: FCS files to get statistics for. If omitted,
+            statistics for all non-control FCS files will be returned.
+        format: str: One of "TSV (with[out] header)", "CSV (with[out] header)" or
+            "json" (default), "pandas", case-insensitive.
+        layout: str: The file (TSV/CSV) or object (JSON) layout. One of "tall-skinny",
+            "medium", or "short-wide".
+        percent_of: str or List[str]: Population ID or array of population IDs.
+            If omitted or the string "PARENT", will calculate percent of parent for
+            each population. If a single ID, will calculate percent of that population
+            for all populations specified by populationIds. If a list, will calculate
+            percent of each of those populations.
+        population_ids: List[str]: List of population IDs. Defaults to ungated.
+    Returns:
+        statistics: Dict, String, or pandas.Dataframe
+    """
     def determine_format(f):
         if f == "pandas":
             return "json"
