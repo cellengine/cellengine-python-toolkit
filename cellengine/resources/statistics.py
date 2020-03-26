184b7bdc0fc5b0c7afb7bbcfcb6e36f2e5124613
gegnew <gerrit.egnew@relayr.io>
2020-03-25 21:12:26 -0600

    switch stats to pure function

--- cellengine/resources/statistics.py
+++ cellengine/resources/statistics.py
@@ -1,125 +1,52 @@
-from io import StringIO
-from cachetools import cachedmethod, LRUCache
-from cachetools.keys import hashkey
-import attr
 import pandas
-from typing import List, Set, Dict, Tuple, Optional, Union
-from cellengine.utils import helpers
-from cellengine.utils.helpers import GetSet, base_post, snake_to_camel
+from typing import Union, List
+from cellengine.utils.helpers import GetSet, base_post
 
-
-def to_tuple(lst):
-    return tuple(to_tuple(i) if isinstance(i, list) else i for i in lst)
-
-def custom_key(*args, env={}):
-    items = list(args[1].values())
-    key = hashkey(to_tuple(items))
-    return key
-
-@attr.s
-class StatisticRequest:
-    """
-    Helper class for requesting Statistics from CellEngine.
-
-    Required Args:
-        experiment_id: ID of experiment to request statistics for.
-        statistics: Statistical method to request. Any of "mean", "median", "quantile",
-            "mad" (median absolute deviation), "geometric_mean", "eventcount",
-            "cv", "stddev" or "percent" (case-insensitive).
-        q: int: quantile (required for "quantile" statistic)
-        channels: List[str]: for "mean", "median", "geometricmean", "cv",
-            "stddev", "mad" or "quantile" statistics. Names of
-            channels to calculate statistics for.
-
-    Optional Args:
-        annotations: bool: Include file annotations in output (defaults to False).
-        compensation_id: str: Compensation to use for gating and statistic calculation.
-            Defaults to uncompensated. Three special constants may be used:
-                0: Uncompensated
-                -1: File-Internal Compensation Uses the file's internal compensation
-                    matrix, if available. If not available, an error will be returned.
-                -2: Per-File Compensation Use the compensation assigned to each
-                    individual FCS file.
-        fcs_file_ids: str or List[str]: FCS files to get statistics for. If omitted,
-            statistics for all non-control FCS files will be returned.
-        format: str: One of "TSV (with[out] header)", "CSV (with[out] header)" or
-            "json" (default), case-insensitive.
-        layout: str: The file (TSV/CSV) or object (JSON) layout. One of "tall-skinny",
-            "medium", or "short-wide".
-        percent_of: str or List[str]: Population ID or array of population IDs.
-            If omitted or the string "PARENT", will calculate percent of parent for
-            each population. If a single ID, will calculate percent of that population
-            for all populations specified by populationIds. If a list, will calculate
-            percent of each of those populations.
-        population_ids: List[str]: List of population IDs. Defaults to ungated.
-
-    Returns:
-        statistics: Dict, String, or pandas.Dataframe (if
-    """
-    experiment_id = attr.ib()
-    statistics = attr.ib(default=None)
-    q = attr.ib(default=None)
-    channels = attr.ib(default=[])
-    annotations = attr.ib(default=False)
-    compensation_id = attr.ib(default=None)
-    fcs_file_ids = attr.ib(default=None)
-    format = attr.ib(default="json")
-    layout = attr.ib(default=None)
-    percent_of = attr.ib(default=None)
-    population_ids = attr.ib(default=None)
-    _data = attr.ib(default=None, repr=False)
-    _cache = attr.ib(default=LRUCache(maxsize=64), repr=False)
-
-    def get(self, method: List[str] = None):
-        if method:
-            return self.make_request(method)
+def get_statistics(
+    experiment_id: str,
+    statistics: Union[str, List[str]],
+    channels: List[str],
+    q: float = None,
+    annotations: bool = False,
+    compensation_id: str = None,
+    fcs_file_ids: List[str] = None,
+    format: str = "json",
+    layout: str = None,
+    percent_of: Union[str, List[str]] = None,
+    population_ids: List[str] = None,
+):
+    def determine_format(f):
+        if f == "pandas":
+            return "json"
         else:
-            try:
-                return self.make_request(self.statistics)
-            except:
-                raise ValueError("You must specify a method (or methods).")
+            return f
 
-    def make_request(self, method):
-        if type(method) is list:
-            self.statistics = [snake_to_camel(item) for item in method]
-        else:
-            self.statistics = snake_to_camel(method)
-        data=self._assemble_parameters()
-        return self._request(method, data)
+    params = {
+        "statistics": statistics,
+        "q": q,
+        "channels": channels,
+        "annotations": annotations,
+        "compensationId": compensation_id,
+        "fcsFileIds": fcs_file_ids,
+        "format": determine_format(format),
+        "layout": layout,
+        "percentOf": percent_of,
+        "populationIds": population_ids,
+    }
+    req_params = {key: val for key, val in params.items() if val is not None}
 
-    def _assemble_parameters(self):
-        params = {
-            "statistics": self.statistics,
-            "q": self.q,
-            "channels": self.channels,
-            "annotations": self.annotations,
-            "compensationId": self.compensation_id,
-            "fcsFileIds": self.fcs_file_ids,
-            "format": self.format,
-            "layout": self.layout,
-            "percentOf": self.percent_of,
-            "populationIds": self.population_ids,
-        }
-        return {key: val for key, val in params.items() if val is not None}
+    res = base_post("experiments/{}/bulkstatistics".format(experiment_id), data=req_params)
 
-
-    @cachedmethod(lambda self: self._cache, key=custom_key)
-    def _request(self, method, data):
-        res = base_post("experiments/{}/bulkstatistics".format(self.experiment_id), data=data)
-        if self.format == "json":
-            self._data = res.json()
-        else:
-            try:
-                self._data = res.content.decode()
-            except:
-                raise ValueError("Invalid output format {}".format(self.format))
-        return self._data
-
-    def to_pandas(self):
+    format = format.lower()
+    if format == "json":
+        return res.json()
+    elif ("sv" in format):
         try:
-            return pandas.DataFrame.from_dict(self._data)
+            return res.content.decode()
+        except ValueError as e:
+            raise e("Invalid output format {}".format(format))
+    elif format == "pandas":
+        try:
+            return pandas.DataFrame.from_dict(res.json())
         except:
-            raise ValueError("Invalid saved data format {} for pandas".format(self.format))
-
-    def cache_clear(self):
-        self._cache.clear()
+            raise ValueError("Invalid data format {} for pandas".format(format))
--- tests/unit/resources/test_statistics.py
+++ tests/unit/resources/test_statistics.py
@@ -4,184 +4,108 @@ import pytest
 import pandas
 import responses
 from requests.exceptions import RequestException
+
 import cellengine
-from cellengine.resources.statistics import StatisticRequest
-from cellengine.utils.helpers import snake_to_camel
+from cellengine.resources.statistics import get_statistics
 
 base_url = os.environ.get("CELLENGINE_DEVELOPMENT", "https://cellengine.com/api/v1/")
 
+
 @pytest.fixture(scope="module")
 def statistic_response(experiment, statistics):
-    # with responses.RequestsMock() as resps:
-    #     resps.add(
-    #         responses.GET,
-    #         base_url + "experiments/5d38a6f79fae87499999a74b/bulkstatistics",
-    #         json=statistics,
-    #     )
     return statistics
-
-
-def statistics_tester(res):
-    assert type(res) is list
-    properties = [
-        'fcsFileId',
-        'filename',
-        'populationId',
-        'population',
-        'uniquePopulationName',
-        'parentPopulation',
-        'parentPopulationId',
-    ]
-    for item in res:
-        assert all(prop in item.keys() for prop in properties)
 
 
 @responses.activate
 def test_object_should_request_all_properties(experiment, statistic_response):
-    params = {
-        "statistics": None,
-        "q": None,
-        "channels": [],
-        "annotations": False,
-        "compensationId": None,
-        "fcsFileIds": None,
-        "format": "json",
-        "layout": None,
-        "percentOf": None,
-        "populationIds": None,
-    }
     responses.add(
-        responses.POST, base_url + "experiments/5e4fcb98bdd7ea051d703652/bulkstatistics", json=statistic_response
+        responses.POST,
+        base_url + "experiments/{}/bulkstatistics".format(experiment._id),
+        json=statistic_response,
     )
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    s.q = 1
-    s.channels.append("FSC-A")
-    s.annotations = False
-    s.compensation_id = "some id"
-    s.fcs_file_ids = "some file id"
-    s.format = "json"
-    s.layout = "medium"
-    s.percent_of = "PARENT"
-    s.population_ids = "some population id"
-
-    s.get("mean")
 
     body = "statistics=mean&q=1&channels=FSC-A&annotations=False&compensationId=some+id&fcsFileIds=some+file+id&format=json&layout=medium&percentOf=PARENT&populationIds=some+population+id"
+    stats = get_statistics(
+        experiment._id,
+        "mean",
+        "FSC-A",
+        q=1,
+        compensation_id="some id",
+        fcs_file_ids="some file id",
+        format="json",
+        layout="medium",
+        percent_of="PARENT",
+        population_ids="some population id",
+    )
+
     assert responses.calls[0].request.body == body
-    s.cache_clear()
 
 
 @pytest.mark.vcr()
-def test_should_get_list_of_stats(statistic_response):
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    s.channels = ['FSC-A']
+def test_should_get_list_of_stats():
     methods_to_get = ["mean", "mad", "stddev"]
-    stats = s.get(methods_to_get)
+    stats = get_statistics("5e4fcb98bdd7ea051d703652", methods_to_get, "FSC-A")
     assert all([method in stats[0].keys() for method in methods_to_get])
-    s.cache_clear()
 
 
 @pytest.mark.vcr()
-def test_should_get_stats_when_called_second_time_without_method(statistic_response):
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    s.get("mean")
-    stats = s.get()
-    statistics_tester(stats)
-    s.cache_clear()
-
-
-@responses.activate
-def test_should_cache_same_request(statistic_response):
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    responses.add(
-        responses.POST, base_url + "experiments/5e4fcb98bdd7ea051d703652/bulkstatistics", json=statistic_response
-    )
-    s.get("mean")
-    s.get("mean")
-    assert len(responses.calls) == 1
-    s.cache_clear()
-
-
-@responses.activate
-def test_should_not_cache_different_request(statistic_response):
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    responses.add(
-        responses.POST, base_url + "experiments/5e4fcb98bdd7ea051d703652/bulkstatistics", json=statistic_response
-    )
-    s.get("mean")
-    s.get("mad")
-    assert len(responses.calls) == 2
-    s.cache_clear()
-
-
-@responses.activate
-def test_should_not_cache_when_params_are_changed(statistic_response):
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    responses.add(
-        responses.POST, base_url + "experiments/5e4fcb98bdd7ea051d703652/bulkstatistics", json=statistic_response
-    )
-    s.channels = ['FSC-A']
-    s.get("mean")
-    s.channels.append('FSC-B')
-    s.get("mean")
-    assert len(responses.calls) == 2
-    s.cache_clear()
+def test_should_get_list_of_stats():
+    channels_to_get = ["FSC-A", "FSC-H"]
+    stats = get_statistics("5e4fcb98bdd7ea051d703652", "mean", channels_to_get)
+    assert any([channels_to_get[0] in stat["channel"] for stat in stats])
+    assert any([channels_to_get[1] in stat["channel"] for stat in stats])
 
 
 @pytest.mark.vcr()
 def test_quantile_should_require_q():
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    s.channels = ['FSC-A']
-    # fails without q set
     with pytest.raises(RequestException):
-        s.get("quantile")
+        get_statistics("5e4fcb98bdd7ea051d703652", "quantile", "FSC-A")
 
     # passes with q set
-    s.q = 0.5
-    stats = s.get("quantile")
-    statistics_tester(stats)
-    s.cache_clear()
+    get_statistics("5e4fcb98bdd7ea051d703652", "quantile", "FSC-A", q=0.75)
 
 
 @pytest.mark.vcr()
 def test_should_get_every_statistics_type():
-    methods = ["mean", "median", "mad", "geometric_mean", "event_count", "cv", "stddev", "percent"]
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    s.channels = ['FSC-A']
+    methods = [
+        "mean",
+        "median",
+        "mad",
+        "geometricMean",
+        "eventCount",
+        "cv",
+        "stddev",
+        "percent",
+    ]
     for method in methods:
-        stats = s.get(method)
-        statistics_tester(stats)
-        assert [snake_to_camel(method) in stat.keys() for stat in stats]
-    s.cache_clear()
+        stats = get_statistics("5e4fcb98bdd7ea051d703652", method, "FSC-A")
+        assert [method in stat.keys() for stat in stats]
 
 
 @pytest.mark.vcr()
 def test_should_get_formatted_csv():
-    s = StatisticRequest("5e4fcb98bdd7ea051d703652")
-    s.format = "csv"
-    s.layout = "short-wide"
-    stats = s.get('mean')
-    # look for rows
-    assert type(stats.find('\r')) == int
-    s.cache_clear()
+    stats = get_statistics(
+        "5e4fcb98bdd7ea051d703652", "mean", "FSC-A", format="csv", layout="short-wide"
+    )
+    # count rows by row delimiter:
+    assert type(stats.find("\r")) == int
 
 
 @responses.activate
 def test_should_return_pandas_dataframe(statistic_response):
     responses.add(
-        responses.POST, base_url + "experiments/5e4fcb98bdd7ea051d703653/bulkstatistics", json=statistic_response
+        responses.POST,
+        base_url + "experiments/5e4fcb98bdd7ea051d703653/bulkstatistics",
+        json=statistic_response,
     )
-    s = StatisticRequest("5e4fcb98bdd7ea051d703653")
-    s.get("mean")
-    stats = s.to_pandas()
+    stats = get_statistics("5e4fcb98bdd7ea051d703653", "mean", "FSC-A", format="pandas")
     properties = [
-        'fcsFileId',
-        'filename',
-        'populationId',
-        'population',
-        'uniquePopulationName',
-        'parentPopulation',
-        'parentPopulationId',
+        "fcsFileId",
+        "filename",
+        "populationId",
+        "population",
+        "uniquePopulationName",
+        "parentPopulation",
+        "parentPopulationId",
     ]
     assert all(prop in stats.columns.to_list() for prop in properties)
-    s.cache_clear()
