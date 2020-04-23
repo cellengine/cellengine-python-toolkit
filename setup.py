a6dbddbdc2950c8a38fa17c4ee7df806298b2ccf
gegnew <gerrit.egnew@relayr.io>
2020-04-23 16:53:22 -0600

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cellengine",
-    version="0.1.0",
+    version="0.2.0",
     description="CellEngine API toolkit for Python",
     url="https://github.com/primitybio/cellengine-python-toolkit",
     author="Primity Bio",
