4a0197ed4d099a1cf493c25850e5fb6f4dc23ff5
gegnew <g.egnew@gmail.com>
2020-11-12 22:41:17 +0100

    Introduce ScaleSet and .apply
    Downgrade pickle protocol

--- cellengine/resources/scaleset.py
+++ cellengine/resources/scaleset.py
@@ -1,5 +1,9 @@
+from pandas import DataFrame
+
 import cellengine as ce
 from cellengine.payloads.scaleset import _ScaleSet
+from cellengine.resources.fcs_file import FcsFile
+from cellengine.payloads.scale_utils.apply_scale import apply_scale
 
 
 class ScaleSet(_ScaleSet):
@@ -9,7 +13,38 @@ class ScaleSet(_ScaleSet):
 
     def update(self):
         """Save changes to this ScaleSet to CellEngine."""
+        self._save_scales()
         res = ce.APIClient().update_entity(
             self.experiment_id, self._id, "scalesets", self._properties
         )
         self._properties.update(res)
+
+    def apply(self, file, clamp_q=False, in_place=True):
+        """Apply the scaleset to a file.
+
+        Args:
+            file (_id or FcsFile): The file to which this scaleset will be
+                applied.
+            clamp_q (bool): Clamp the output to the scale's minimum and maximum
+                values.
+            in_place (bool): If True, updates the FcsFile.events; if
+                False, returns a DataFrame
+        """
+        if type(file) is not FcsFile:
+            file = FcsFile.get(file)
+
+        data = file.events
+        dest = DataFrame()
+
+        for channel, scale in self.scales.items():
+            dest[channel] = data[channel].map(lambda a: apply_scale(scale, a, clamp_q))
+        if in_place:
+            file.events = dest
+        else:
+            return dest
+
+    def _save_scales(self):
+        """Save changes to scales to the object-internal _properties"""
+        self._properties["scales"] = [
+            {"channelName": key, "scale": val} for key, val in self.scales.items()
+        ]
---
+++ cellengine/utils/limited_dict.py
@@ -0,0 +1,17 @@
+class LimitedDict(dict):
+    """ Extend this class and define `_keys` to limit the keys
+    available in a dict.
+    """
+
+    _keys = set()
+
+    def __init__(self, init_dict):
+        for key in init_dict.keys():
+            if key not in self._keys:
+                raise ValueError(f"'{key}' is not a valid key.")
+        dict.__init__(self, init_dict)
+
+    def __setitem__(self, key, val):
+        if key not in self._keys:
+            raise KeyError(f"'{key}' is not a valid key.")
+        dict.__setitem__(self, key, val)
