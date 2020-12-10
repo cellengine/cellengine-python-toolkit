70a146637417a78e5cf227c626cd1613d2e0b93d
gegnew <g.egnew@gmail.com>
2020-12-10 18:35:54 +0100

    Update docstrings for gate formatting funcs

--- cellengine/payloads/gate_utils/split_gate.py
+++ cellengine/payloads/gate_utils/split_gate.py
@@ -27,13 +27,39 @@ def format_split_gate(
     each with a unique gid and name.
 
     Args:
+        experiment_id: The ID of the experiment to which to add the gate.
+            Use when calling this as a static method; not needed when calling
+            from an Experiment object
+        x_channel: The name of the x channel to which the gate applies.
+        name: The name of the gate
         x: The x coordinate of the center point (after the channel's scale has
             been applied).
         y: The relative position from 0 to 1 of the dashed line extending
            from the center point.
         labels: Positions of the quadrant labels. A list of two length-2 lists in
             the order: L, R. These are set automatically to the top corners.
+        gid: Group ID of the gate, used for tailoring. If this is not
+            specified, then a new Group ID will be created. To create a
+            tailored gate, the gid of the global tailored gate must be specified.
         gids: Group IDs of each sector, assigned to model.gids.
+        locked: Prevents modification of the gate via the web interface.
+        parent_population_id: ID of the parent population. Use ``None`` for
+            the "ungated" population. If specified, do not specify
+            ``parent_population``.
+        parent_population: Name of the parent population. An attempt will
+            be made to find the population by name.  If zero or more than
+            one population exists with the name, an error will be thrown.
+            If specified, do not specify ``parent_population_id``.
+        tailored_per_file: Whether or not this gate is tailored per FCS file.
+        fcs_file_id: ID of FCS file, if tailored per file. Use ``None`` for
+            the global gate in a tailored gate group. If specified, do not
+            specify ``fcs_file``.
+        fcs_file: Name of FCS file, if tailored per file. An attempt will be made
+            to find the file by name. If zero or more than one file exists with
+            the name, an error will be thrown. Looking up files by name is
+            slower than using the ID, as this requires additional requests
+            to the server. If specified, do not specify ``fcs_file_id``.
+        create_population: Automatically create corresponding population.
 
     Returns:
         A SplitGate object.
