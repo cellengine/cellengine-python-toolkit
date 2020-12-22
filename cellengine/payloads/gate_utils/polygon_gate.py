fedefefedcc43c8a0db51e44770891d7aebc546f
gegnew <g.egnew@gmail.com>
2020-12-22 17:34:04 +0100

    Add type annotations for gating functions

--- cellengine/payloads/gate_utils/polygon_gate.py
+++ cellengine/payloads/gate_utils/polygon_gate.py
@@ -23,35 +23,35 @@ def format_polygon_gate(
     """Formats a polygon gate for posting to the CE API.
 
     Args:
-        experiment_id: The ID of the experiment to which to add the gate.
+        experiment_id (str): The ID of the experiment to which to add the gate.
             Use when calling this as a static method; not needed when calling
             from an Experiment object
-        x_channel: The name of the x channel to which the gate applies.
-        y_channel: The name of the y channel to which the gate applies.
-        vertices: List of coordinates, like [[x,y], [x,y], ...]
-        label: Position of the label. Defaults to the midpoint of the gate.
-        name: The name of the gate
-        gid: Group ID of the gate, used for tailoring. If this is not
+        x_channel (str): The name of the x channel to which the gate applies.
+        y_channel (str): The name of the y channel to which the gate applies.
+        vertices (list): List of coordinates, like [[x,y], [x,y], ...]
+        label (str): Position of the label. Defaults to the midpoint of the gate.
+        name (str): The name of the gate
+        gid (str): Group ID of the gate, used for tailoring. If this is not
             specified, then a new Group ID will be created. To create a
             tailored gate, the gid of the global tailored gate must be specified.
-        locked: Prevents modification of the gate via the web interface.
-        parent_population_id: ID of the parent population. Use ``None`` for
+        locked (bool): Prevents modification of the gate via the web interface.
+        parent_population_id (str): ID of the parent population. Use ``None`` for
             the "ungated" population. If specified, do not specify
             ``parent_population``.
-        parent_population: Name of the parent population. An attempt will
+        parent_population (str): Name of the parent population. An attempt will
             be made to find the population by name.  If zero or more than
             one population exists with the name, an error will be thrown.
             If specified, do not specify ``parent_population_id``.
-        tailored_per_file: Whether or not this gate is tailored per FCS file.
-        fcs_file_id: ID of FCS file, if tailored per file. Use ``None`` for
+        tailored_per_file (bool): Whether or not this gate is tailored per FCS file.
+        fcs_file_id (str): ID of FCS file, if tailored per file. Use ``None`` for
             the global gate in a tailored gate group. If specified, do not
             specify ``fcs_file``.
-        fcs_file: Name of FCS file, if tailored per file. An attempt will be made
+        fcs_file (str): Name of FCS file, if tailored per file. An attempt will be made
             to find the file by name. If zero or more than one file exists with
             the name, an error will be thrown. Looking up files by name is
             slower than using the ID, as this requires additional requests
             to the server. If specified, do not specify ``fcs_file_id``.
-        create_population: Automatically create corresponding population.
+        create_population (bool): Automatically create corresponding population.
 
     Returns:
         A PolygonGate object.
