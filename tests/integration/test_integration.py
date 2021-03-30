420862bd2125ef3a769d5045939e7099360911f6
gegnew <g.egnew@gmail.com>
2021-03-30 12:45:58 +0200

    Simplify integration tests, add new test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,4 +1,3 @@
-import logging
 import pytest
 import pandas
 
@@ -17,219 +16,224 @@ from cellengine.resources.gate import (
 from cellengine.utils.complex_population_builder import ComplexPopulationBuilder
 
 
-log = logging.getLogger(__name__)
-
-
 @pytest.fixture(scope="module")
 def client():
-    return cellengine.APIClient("gegnew", "^w^A7kpB$2sezF")
+    return cellengine.APIClient("gegnew", "testpass123")
 
 
 @pytest.fixture(scope="module")
 def setup_experiment(request, client):
-    log.info("Setting up CellEngine experiment for {}".format(__name__))
-    # exp = cellengine.Experiment.create("new_experiment")
-    # exp.upload_fcs_file("tests/data/Acea - Novocyte.fcs")
+    print("Setting up CellEngine experiment for {}".format(__name__))
+    exp = cellengine.Experiment.create("new_experiment")
+    exp.upload_fcs_file("tests/data/Acea - Novocyte.fcs")
 
-    # def teardown():
-    #     log.info("Starting teardown of: {}".format(__name__))
-    #     client.delete_experiment(exp._id)
+    yield exp
 
-    # request.addfinalizer(teardown)
-
-
-class TestExperimentIntegration:
-    def test_experiment_attachments(self, setup_experiment, client):
-        experiment = client.get_experiment(name="new_experiment")
-
-        # POST
-        experiment.upload_attachment("tests/data/text.txt")
-        experiment.upload_attachment("tests/data/text.txt", filename="text2.txt")
-
-        # GET
-        attachments = experiment.attachments
-        assert all([type(a) is Attachment for a in attachments])
-        assert len(attachments) == 2
-        assert experiment.download_attachment(name="text2.txt") == b"hello world\n\n"
-        att1 = experiment.get_attachment(name="text.txt")
-        experiment.get_attachment(_id=experiment.attachments[1]._id)
-
-        # UPDATE
-        att1.filename = "newname.txt"
-        att1.update()
-        assert experiment.download_attachment(name="newname.txt") == b"hello world\n\n"
-
-        # DELETE
-        [a.delete() for a in experiment.attachments]
-        assert len(experiment.attachments) == 0
-
-    def test_fcs_file_events(self, setup_experiment, client):
-        experiment = client.get_experiment(name="new_experiment")
-        file = experiment.fcs_files[0]
-
-        all_events = file.events
-        limited_events = file.get_events(preSubsampleN = 10)
-
-        assert len(all_events) > len(limited_events)
-        assert len(limited_events) == 10
-
-        # it should update the events value
-        file.get_events(preSubsampleN = 10, inplace=True)
-        assert len(limited_events) == len(file.events)
+    print("Starting teardown of: {}".format(__name__))
+    client.delete_experiment(exp._id)
 
 
-    def test_experiment_compensations(self, setup_experiment, client):
-        experiment = client.get_experiment(name="new_experiment")
+def test_experiment_attachments(setup_experiment, client):
+    experiment = client.get_experiment(name="new_experiment")
 
-        # POST
-        file1 = experiment.fcs_files[0]
-        experiment.create_compensation("test_comp", file1.channels[0:2], [1, 2, 3, 4])
+    # POST
+    experiment.upload_attachment("tests/data/text.txt")
+    experiment.upload_attachment("tests/data/text.txt", filename="text2.txt")
 
-        # GET
-        compensations = experiment.compensations
-        assert all([type(c) is Compensation for c in compensations])
-        comp = compensations[0]
-        assert comp.name == "test_comp"
+    # GET
+    attachments = experiment.attachments
+    assert all([type(a) is Attachment for a in attachments])
+    assert len(attachments) == 2
+    assert experiment.download_attachment(name="text2.txt") == b"hello world\n\n"
+    att1 = experiment.get_attachment(name="text.txt")
+    experiment.get_attachment(_id=experiment.attachments[1]._id)
 
-        # UPDATE
-        comp.name = "new_name"
-        comp.update()
-        comp = experiment.get_compensation(name="new_name")
+    # UPDATE
+    att1.filename = "newname.txt"
+    att1.update()
+    assert experiment.download_attachment(name="newname.txt") == b"hello world\n\n"
 
-        # Additional functionality
-        events_df = comp.apply(file1, inplace=False, preSubsampleN=100)
-        assert len(events_df) == 100
-        assert type(comp.dataframe) == pandas.DataFrame
+    # DELETE
+    [a.delete() for a in experiment.attachments]
+    assert len(experiment.attachments) == 0
 
-        # DELETE
-        experiment.compensations[0].delete()
-        assert experiment.compensations == []
 
-    def test_experiment(self, setup_experiment, client):
-        # POST
-        exp = cellengine.Experiment.create("new_experiment_2")
-        exp.upload_fcs_file("tests/data/Acea - Novocyte.fcs")
+def test_fcs_file_events(setup_experiment, client):
+    experiment = client.get_experiment(name="new_experiment")
+    file = experiment.fcs_files[0]
 
-        # GET
-        experiments = client.get_experiments()
-        # assert len(experiments) == 2
-        assert all([type(exp) is Experiment for exp in experiments])
-        exp2 = client.get_experiment(name="new_experiment_2")
+    all_events = file.events
+    limited_events = file.get_events(preSubsampleN=10)
 
-        # UPDATE
-        clone = exp2.clone(name="clone")
-        clone.name = "edited_experiment"
-        clone.update()
-        assert clone.name == "edited_experiment"
+    assert len(all_events) > len(limited_events)
+    assert len(limited_events) == 10
 
-        # DELETE
-        client.delete_experiment(exp._id)
-        client.delete_experiment(clone._id)
-        with pytest.raises(APIError):
-            client.get_experiment(exp._id)
+    # it should update the events value
+    file.get_events(preSubsampleN=10, inplace=True)
+    assert len(limited_events) == len(file.events)
 
-    def test_experiment_fcs_files(self, setup_experiment, client):
-        experiment = client.get_experiment(name="new_experiment")
 
-        # GET
-        files = experiment.fcs_files
-        assert all([type(f) is FcsFile for f in files])
+def test_experiment_compensations(setup_experiment, client):
+    experiment = client.get_experiment(name="new_experiment")
 
-        # CREATE
-        file2 = FcsFile.create(
-            experiment._id,
-            fcs_files=files[0]._id,
-            filename="new_fcs_file.fcs",
-            pre_subsample_n=10,
-        )
+    # POST
+    file1 = experiment.fcs_files[0]
+    experiment.create_compensation("test_comp", file1.channels[0:2], [1, 2, 3, 4])
 
-        # UPDATE
-        file2.filename = "renamed.fcs"
-        file2.update()
-        file = experiment.get_fcs_file(name="renamed.fcs")
-        assert file._id == file2._id
+    # GET
+    compensations = experiment.compensations
+    assert all([type(c) is Compensation for c in compensations])
+    comp = compensations[0]
+    assert comp.name == "test_comp"
 
-        # events
-        events_df = file.get_events(preSubsampleN=10, seed=7)
-        assert len(events_df) == 10
+    # UPDATE
+    comp.name = "new_name"
+    comp.update()
+    comp = experiment.get_compensation(name="new_name")
 
-        # DELETE
-        file.delete()
-        with pytest.raises(APIError):
-            client.get_fcs_file(experiment._id, file._id)
+    # Additional functionality
+    events_df = comp.apply(file1, preSubsampleN=100)
+    assert len(events_df) == 100
+    assert type(comp.dataframe) == pandas.DataFrame
 
-    def test_experiment_gates(self, setup_experiment, client):
-        experiment = client.get_experiment(name="new_experiment")
-        fcs_file = experiment.fcs_files[0]
+    # with inplace=True it should save results to the target FcsFile
+    another_events_df = comp.apply(file1, inplace=True, preSubsampleN=10)
+    file1.events == another_events_df
 
-        # CREATE
-        split_gate = SplitGate.create(
-            experiment._id, fcs_file.channels[0], "split_gate", 2300000, 250000
-        )
-        range_gate = RangeGate.create(
-            experiment._id,
-            fcs_file.channels[0],
-            "range_gate",
-            2100000,
-            2500000,
-            gid=split_gate.gid,
-        )
+    # DELETE
+    experiment.compensations[0].delete()
+    assert experiment.compensations == []
 
-        # UPDATE
-        range_gate.tailor_to(fcs_file._id)
-        range_gate.update()
-        assert range_gate.fcs_file_id == fcs_file._id
 
-        population = experiment.populations[0]
-        Gate.update_gate_family(
-            experiment._id,
-            split_gate.gid,
-            body={"name": "new split gate name", "parentPopulationId": population._id},
-        )
-        assert experiment.gates[0].parent_population_id == population._id
-        assert experiment.gates[0].name == "new split gate name"
+def test_experiment(setup_experiment, client):
+    # POST
+    exp = cellengine.Experiment.create("new_experiment_2")
+    exp.upload_fcs_file("tests/data/Acea - Novocyte.fcs")
 
-        # DELETE
-        range_gate.delete()
-        assert len(experiment.gates) == 1
+    # GET
+    experiments = client.get_experiments()
+    # assert len(experiments) == 2
+    assert all([type(exp) is Experiment for exp in experiments])
+    exp2 = client.get_experiment(name="new_experiment_2")
 
-        Gate.delete_gates(experiment._id, gid=split_gate.gid)
-        assert experiment.gates == []
+    # UPDATE
+    clone = exp2.clone(name="clone")
+    clone.name = "edited_experiment"
+    clone.update()
+    assert clone.name == "edited_experiment"
 
-    def test_experiment_populations(self, setup_experiment, client):
-        experiment = client.get_experiment(name="new_experiment")
-        fcs_file = experiment.fcs_files[0]
+    # DELETE
+    client.delete_experiment(exp._id)
+    client.delete_experiment(clone._id)
+    with pytest.raises(APIError):
+        client.get_experiment(exp._id)
 
-        # GET
-        quad_gate = QuadrantGate.create(
-            experiment._id,
-            fcs_file.channels[0],
-            fcs_file.channels[1],
-            "quadrant_gate",
-            2300000,
-            250000,
-        )
-        SplitGate.create(
-            experiment._id, fcs_file.channels[0], "split_gate", 2300000, 250000
-        )
-        assert "split_gate (L)", "split_gate (R)" in [
-            p.name for p in experiment.populations
-        ]
 
-        # CREATE
-        complex_payload = (
-            ComplexPopulationBuilder("complex pop")
-            .Or([quad_gate.model.gids[0], quad_gate.model.gids[2]])
-            .build()
-        )
-        client.post_population(experiment._id, complex_payload)
-        complex_pop = [p for p in experiment.populations if "complex pop" in p.name][0]
+def test_experiment_fcs_files(setup_experiment, client):
+    experiment = client.get_experiment(name="new_experiment")
 
-        # UPDATE
-        complex_pop.terminal_gate_gid = "some other gid"
-        complex_pop.update()
-        assert complex_pop.terminal_gate_gid == "some other gid"
+    # GET
+    files = experiment.fcs_files
+    assert all([type(f) is FcsFile for f in files])
 
-        # DELETE
-        complex_pop.delete()
-        assert "complex pop" not in [p.name for p in experiment.populations]
+    # CREATE
+    file2 = FcsFile.create(
+        experiment._id,
+        fcs_files=files[0]._id,
+        filename="new_fcs_file.fcs",
+        pre_subsample_n=10,
+    )
+
+    # UPDATE
+    file2.filename = "renamed.fcs"
+    file2.update()
+    file = experiment.get_fcs_file(name="renamed.fcs")
+    assert file._id == file2._id
+
+    # events
+    events_df = file.get_events(preSubsampleN=10, seed=7)
+    assert len(events_df) == 10
+
+    # DELETE
+    file.delete()
+    with pytest.raises(APIError):
+        client.get_fcs_file(experiment._id, file._id)
+
+
+def test_experiment_gates(setup_experiment, client):
+    experiment = client.get_experiment(name="new_experiment")
+    fcs_file = experiment.fcs_files[0]
+
+    # CREATE
+    split_gate = SplitGate.create(
+        experiment._id, fcs_file.channels[0], "split_gate", 2300000, 250000
+    )
+    split_gate.post()
+    range_gate = RangeGate.create(
+        experiment._id, fcs_file.channels[0], "range_gate", 2100000, 2500000,
+    )
+    range_gate.post()
+
+    # UPDATE
+    range_gate.tailor_to(fcs_file._id)
+    range_gate.update()
+    assert range_gate.tailored_per_file is True
+    assert range_gate.fcs_file_id == fcs_file._id
+
+    population = experiment.populations[0]
+    Gate.update_gate_family(
+        experiment._id,
+        split_gate.gid,
+        body={"name": "new split gate name", "parentPopulationId": population._id},
+    )
+    assert experiment.gates[0].parent_population_id == population._id
+    assert experiment.gates[0].name == "new split gate name"
+
+    # DELETE
+    range_gate.delete()
+    assert len(experiment.gates) == 1
+
+    Gate.delete_gates(experiment._id, gid=split_gate.gid)
+    assert experiment.gates == []
+
+
+def test_experiment_populations(setup_experiment, client):
+    experiment = client.get_experiment(name="new_experiment")
+    fcs_file = experiment.fcs_files[0]
+
+    # GET
+    quad_gate = QuadrantGate.create(
+        experiment._id,
+        fcs_file.channels[0],
+        fcs_file.channels[1],
+        "quadrant_gate",
+        2300000,
+        250000,
+    )
+    quad_gate.post()
+
+    split_gate = SplitGate.create(
+        experiment._id, fcs_file.channels[0], "split_gate", 2300000, 250000
+    )
+    split_gate.post()
+    assert "split_gate (L)", "split_gate (R)" in [
+        p.name for p in experiment.populations
+    ]
+
+    # CREATE
+    complex_payload = (
+        ComplexPopulationBuilder("complex pop")
+        .Or([quad_gate.model.gids[0], quad_gate.model.gids[2]])
+        .build()
+    )
+    client.post_population(experiment._id, complex_payload)
+    complex_pop = [p for p in experiment.populations if "complex pop" in p.name][0]
+
+    # UPDATE
+    complex_pop.name = "my new name"
+    complex_pop.update()
+    assert complex_pop.name == "my new name"
+
+    # DELETE
+    complex_pop.delete()
+    assert "complex pop" not in [p.name for p in experiment.populations]
