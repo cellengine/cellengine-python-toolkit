980bbd9b405e6b69f125b1dc2ed0cd6f8b94de6b
gegnew <g.egnew@gmail.com>
2021-09-27 20:54:32 +0200

    bug(actions): fix mkdocs warnings in GH Actions

--- cellengine/utils/api_client/APIClient.py
+++ cellengine/utils/api_client/APIClient.py
@@ -506,6 +506,48 @@ class APIClient(BaseAPIClient, metaclass=Singleton):
         percent_of: Optional[Union[str, List[str]]] = "PARENT",
         population_ids: Optional[List[str]] = None,
     ):
+        """
+        Request Statistics from CellEngine.
+
+        Args:
+            experiment_id: ID of the experiment.
+            statistics: Statistical method to request. Any of "mean", "median",
+                "quantile", "mad" (median absolute deviation), "geometricmean",
+                "eventcount", "cv", "stddev" or "percent" (case-insensitive).
+            q (int): quantile (required for "quantile" statistic)
+            channels (Union[str, List[str]]): for "mean", "median", "geometricMean",
+                "cv", "stddev", "mad" or "quantile" statistics. Names of channels
+                to calculate statistics for.
+            annotations: Include file annotations in output
+                (defaults to False).
+            compensation_id: Compensation to use for gating and
+                statistic calculation.
+                Defaults to uncompensated. Three special constants may be used:
+                    0: Uncompensated
+                    -1: File-Internal Compensation Uses the file's internal
+                        compensation matrix, if available. If not, an error
+                        will be returned.
+                    -2: Per-File Compensation Use the compensation assigned to
+                        each individual FCS file.
+            fcs_file_ids: FCS files to get statistics for. If
+                omitted, statistics for all non-control FCS files will be returned.
+            format: str: One of "TSV (with[out] header)",
+                "CSV (with[out] header)" or "json" (default), "pandas",
+                case-insensitive.
+            layout: str: The file (TSV/CSV) or object (JSON) layout.
+                One of "tall-skinny", "medium", or "short-wide".
+            percent_of: str or List[str]: Population ID or array of
+                population IDs.  If omitted or the string "PARENT", will calculate
+                percent of parent for each population. If a single ID, will calculate
+                percent of that population for all populations specified by
+                population_ids. If a list, will calculate percent of each of
+                those populations.
+            population_ids: List[str]: List of population IDs.
+                Defaults to ungated.
+        Returns:
+            statistics: Dict, String, or pandas.Dataframe
+        """
+
         def determine_format(f):
             if f == "pandas":
                 return "json"
