3a0aea07d30c08c6d7a9dd34a95038200d09b2ca
gegnew <g.egnew@gmail.com>
2021-11-15 13:32:05 +0100

    src(plots): update image opener

--- cellengine/utils/wrapped_image_opener.py
+++ cellengine/utils/wrapped_image_opener.py
@@ -1,4 +1,3 @@
-import importlib.util
 from io import BytesIO
 
 from cellengine.utils.singleton import AbstractSingleton
@@ -8,27 +7,29 @@ class WrappedImageOpener(metaclass=AbstractSingleton):
     """Display images with an already-installed image library, if possible"""
 
     def __init__(self):
-        imager = self.find_one_of_packages(["PIL.Image", "IPython.display"])
-        if imager:
-            self.imager = imager
-        else:
-            raise ImportError(
-                "Try installing Pillow with `pip install pillow` \
-                or run in a Jupyter Notebook."
-            )
+        self.imager = self.get_imager()
 
     def open(self, data):
         if "PIL" in str(self.imager):
-            image = self.imager.open(BytesIO(data))
+            image = self.imager.open(BytesIO(data))  # type: ignore
         elif "IPython" in str(self.imager):
-            image = self.imager.display(self.imager.Image(data))
+            image = self.imager.display(self.imager.Image(data))  # type: ignore
+        else:
+            raise RuntimeError("Pillow or IPython is not installed.")
         return image
 
-    def find_one_of_packages(self, package_names):
-        """Find an image library or return None"""
-        for package in package_names:
+    def get_imager(self):
+        try:
+            from PIL import Image
+
+            return Image
+        except ModuleNotFoundError:
             try:
-                lib = importlib.util.find_spec(package)
-                return lib.loader.load_module()
-            except Exception:
-                continue
+                from IPython import display
+
+                return display
+            except ModuleNotFoundError:
+                raise ImportError(
+                    "Try installing Pillow with `pip install pillow` \
+                    or run in a Jupyter Notebook."
+                )
